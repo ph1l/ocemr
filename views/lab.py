ed5221c34ef1abd4b11f3c8c7bd072d93f38132c
Philip Freeman <philip.freeman@gmail.com>
2011-01-05 10:50:02 -0600

    redesign lab_queue

--- views/lab.py
+++ views/lab.py
@@ -39,9 +39,11 @@ def lab_queue(request):
 
 	from ocemr.models import Lab
 
-	past_24h_q = Q(orderedDateTime__gt=d_yesterday)
+	active_q = Q(status='ORD' ) | Q(status='PEN' )
+	inactive_q = Q(orderedDateTime__gt=d_yesterday) & ( Q(status='CAN' ) | Q(status='COM' ) | Q(status='FAI') )
 
-	labs = Lab.objects.filter(past_24h_q).order_by('-orderedDateTime', '-id')
+	labs_active = Lab.objects.filter(active_q).order_by('-orderedDateTime', '-id')
+	labs_inactive = Lab.objects.filter(inactive_q).order_by('-orderedDateTime', '-id')
 	return render_to_response('lab_queue.html', locals(),context_instance=RequestContext(request))
 
 @login_required
