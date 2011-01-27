f6bd2fedaad29db84f57116409723d946db82760
Philip Freeman <philip.freeman@gmail.com>
2011-01-27 10:20:50 -0600

    added shell script to build a source package

---
+++ util/make_package.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+##########################################################################
+#
+#    This file is part of OCEMR.
+#
+#    OCEMR is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    OCEMR is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with OCEMR.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#########################################################################
+#       Copyright 2011 Philip Freeman <philip.freeman@gmail.com>
+##########################################################################
+
+b=`git branch | grep ^\* | cut -b3-`
+t=`git tag -l | tail -1`
+
+if [ "${b}" == "master" ]; then
+    OCEMR_VERSION="${t}"
+else
+    OCEMR_VERSION="${t}-${b}"
+fi
+
+git archive master --prefix=ocemr-${OCEMR_VERSION}/ | bzip2 > ../ocemr-${OCEMR_VERSION}.tar.bz2
+
